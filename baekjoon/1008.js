6c9ca0bc91a71b0a2e121a1ae0f948ec94bc927a
Seongjae <robbyl95@hanmail.net>
2021-12-07 17:18:21 +0900

    go로도 해보기. 폴더링

--- baekjoon/1008.js
+++ baekjoon/1008.js
@@ -1,6 +1,13 @@
-var fs = require("fs");
-var input = fs.readFileSync("/dev/stdin").toString().split(" ");
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
+// var fs = require("fs");
+// var input = fs.readFileSync("/dev/stdin").toString().split(" ");
+// var a = parseInt(input[0]);
+// var b = parseInt(input[1]);
 
-console.log(a / b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// 틀렸다리.... 다시
+
