3af10b715921d76d47468a8277df38208a1dc754
Seongjae <robbyl95@hanmail.net>
2021-12-07 17:19:41 +0900

    bufio에 대한 아이디어(속도 향상)

--- baekjoon/1000/1000.go
+++ baekjoon/1000/1000.go
@@ -9,3 +9,29 @@ func main() {
 	fmt.Scanf("%d %d", &a, &b)
 	fmt.Scanln(a + b)
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// ★★★ bufio로 속도 올리기!!!!
+//  package main
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
+
+// var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+// var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+// func main() {
+// 	defer writer.Flush()
+
+// 	var T int
+//     fmt.Fscan(reader, &T)
+// 	for i := 0; i < T; i++ {
+// 		var A, B int
+//         fmt.Fscan(reader, &A, &B)
+//         fmt.Fprintln(writer, A+B)
+// 	}
+// }
